b9a39a835bce913654c75ddbd198cce1939438dd
Hrbolek <alexandr.stefek@unob.cz>
2023-11-03 07:41:40 +0100

    step 10

--- GraphTypeDefinitions/permissions.py
+++ GraphTypeDefinitions/permissions.py
@@ -3,14 +3,14 @@ import strawberry
 from strawberry.permission import BasePermission
 from strawberry.types import Info
 
-from utils.Dataloaders import getUser
+from utils.Dataloaders import getUserFromInfo
 
 class SensitiveInfo(BasePermission):
     message = "User is not allowed to read sensitive info"
 
     # This method can also be async!
     async def has_permission(self, source: typing.Any, info: Info, **kwargs) -> bool:
-        user = getUser(info)
+        user = getUserFromInfo(info)
         if user is None:
             return False
         if user["id"] == "2d9dc5ca-a4a2-11ed-b9df-0242ac120003":
--- tests/test_client.py
+++ tests/test_client.py
@@ -6,7 +6,7 @@ from .client import createGQLClient
 def test_client_read():
     client = createGQLClient()
     json = {
-        'query': """query($id: UUID!){ eventById(id: $id) {id} }""",
+        'query': """query($id: UUID!){ result: eventById(id: $id) {id} }""",
         'variables': {
             'id': '45b2df80-ae0f-11ed-9bd8-0242ac110002'
         }
@@ -19,4 +19,50 @@ def test_client_read():
     assert response.get("error", None) is None
     data = response.get("data", None)
     assert data is not None
+    #assert False
+
+
+
+def test_client_auth_ok():
+    client = createGQLClient()
+    json = {
+        'query': """query($id: UUID!){ result: eventById(id: $id) { id sensitiveMsg }}""",
+        'variables': {
+            'id': '45b2df80-ae0f-11ed-9bd8-0242ac110002'
+        }
+    }
+    headers = {"Authorization": "Bearer 2d9dc5ca-a4a2-11ed-b9df-0242ac120003"}
+    response = client.post("/gql", headers=headers, json=json)
+    assert response.status_code == 200
+    response = response.json()
+    print(response)
+    assert response.get("error", None) is None
+    data = response.get("data", None)
+    assert data is not None
+    result = data.get("result", None)
+    assert result is not None
+    sensitiveMsg = result.get("sensitiveMsg", None)
+    assert sensitiveMsg is not None
+    #assert False
+
+def test_client_auth_notok():
+    client = createGQLClient()
+    json = {
+        'query': """query($id: UUID!){ result: eventById(id: $id) { id sensitiveMsg }}""",
+        'variables': {
+            'id': '45b2df80-ae0f-11ed-9bd8-0242ac110002'
+        }
+    }
+    headers = {}
+    response = client.post("/gql", headers=headers, json=json)
+    assert response.status_code == 200
+    response = response.json()
+    print(response)
+    assert response.get("error", None) is None
+    data = response.get("data", None)
+    assert data is not None
+    result = data.get("result", None)
+    assert result is not None
+    sensitiveMsg = result.get("sensitiveMsg", None)
+    assert sensitiveMsg is None
     #assert False
